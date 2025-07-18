b3cbf53e45dcf625038f591ed90059a08ab4553d
Audri Saulters <audri@Audris-MacBook-Air-4.local>
2025-07-18 15:03:51 +0100

    Set up node structures

---
+++ Shape.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+var rarity: float = 0.5 # value between 0-1
+var form: 
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
